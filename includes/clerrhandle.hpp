84b3c25180e09fca08193a47d42a7f71b3fc54f1
vimal <vimalwilliam99@gmail.com>
2023-06-10 12:40:15 +0530

    updated the error handles for opencl based hw detector

--- includes/clerrhandle.hpp
+++ includes/clerrhandle.hpp
@@ -1,3 +1,23 @@
 #include <CL/opencl.hpp>
 #include <bits/stdc++.h>
 
+class ErrorHandle{
+    private:
+        cl_int err; 
+    
+    public:
+        ErrorHandle(cl_int error){
+            err = error; 
+            Handler();
+        }
+
+        void Handler(){
+            switch(err){
+                case CL_INVALID_VALUE:
+                    std::cerr << "Error: The Platform is Null"; 
+                
+                default:
+                    std::cerr << "Un-Identified Error Flag" << std::endl;
+            }
+        }
+}
