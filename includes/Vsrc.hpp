cd6cbc448e9ff1400c3c8428c02f787251efa083
vimal <vimalwilliam99@gmail.com>
2023-06-10 16:12:32 +0530

    appsrc setted and the memory is allocated to Gst buffer

--- includes/Vsrc.hpp
+++ includes/Vsrc.hpp
@@ -1,30 +1,96 @@
-/*Vsrc - handles various source and converting to frames,
-Pre - processing the frames and Push to AI Inference on it,
-further push to GStreamer buffer*/
+#include <CL/opencl.hpp>
+#include <bits/stdc++.h>
+
+#include <includes/clerrhandle.hpp>
+#include <opencv2/opencv.hpp>
 
 #include <gst/gst.h>
 #include <gst/app/gstappsrc.h>
 #include <gst/app/app.h>
-#include <opencv4/opencv2/opencv.hpp>
 
-#include <bits/stdc++.h>
+class HWDetector{
+    private:
+        std::vector<cl::Platform> platforms; 
+        std::vector<cl::Device> devices, ctxDevices;
 
-struct userVar{
-    std::string SourceName; 
-    cv::VideoCapture cap; 
-}vars; 
+        std::string DeviceName;
+        std::string HW = "NVIDIA";
+        cl_int err;
 
-//analyse the source and return in the valid type
-//initally the source is treated as string 
-auto sourceAnalyzer(){
-    
+        bool HWFlag = false;
+
+    public:
+        HWDetector(){}
+
+        void Detector(){
+
+            //get the context and ctx device 
+            cl::Platform(&platforms); 
+            err = platforms[0].getInfo(CL_DEVICE_TYPE_GPU, &devices); 
+
+            if (err != CL_SUCCESS){
+                ErrorHandle handle(err); 
+            }
+
+            err = cl::Context context(devices); 
+            if (err != CL_SUCCESS){
+                ErrorHandle handle(err);
+            }
+
+            //get the device from the context
+            ctxDevices = context.getInfo<CL_CONTEXT_DEVICES>(); 
+            DeviceName = ctxDevices[0].getInfo<CL_DEVICE_NAME>(); 
+
+            //search for NVIDIA GPU support
+            if(DeviceName.find(HW) != std::string::npos){
+                std::cout << "The NVIDIA GPU accelerator detected" << std::endl;
+                HWFlag = true; 
+            }else {
+                std::cout << "Warning: The particular accelerator not detected" << std::endl; 
+                HWFlag = false;
+            }
+        }
 }
 
-static void NeedSource(GstAppSrc *appsrc, gpointer userData){
-    //initalize the gstreamer handles 
-    GstBuffer *Buffer; 
-    GstFlowReturn ret; 
-    GstMapInfo map; 
-    vars.cap()
+class SrcHandle{
+    private:
+        GstBuffer* buffer; 
+        GstMapInfo map; 
+        GstFlowReturn ret; 
+        GMainLoop* loop;
 
+        //source handle params 
+        std::string Srcloc; 
+        char SrcType;
+    
+    public:
+        SrcHandle(std::string SrcDir, char SrcCl){
+            Srcloc = SrcDir; 
+            SrcType = SrcCl;
+
+        }
+
+        static void NeedSource(GstAppSrc *appsrc, gpointer user_data){
+            
+            //get the appsrc and push to buffer
+            if(SrcType == "i"){
+                auto src_int = int(Srcloc); 
+
+                cv::VideoCapture cap(src_int); 
+                while(cap.isOpened()){
+                    cv::Mat frame; 
+                    cap.read(frame); 
+                    
+                    size_t size = frame.cols * frame.rows * frame.channels(); 
+                    buffer = gst_buffer_new_allocate(NULL, size, NULL); 
+                    gst_buffer_map(buffer, &map, GST_MAP_WRITE); 
+
+                    //AI Inference and frame overlay function
+                    
+                    memcpy(map.data, frame.data, size); 
+
+
+                }
+            }  
+        }
 }
