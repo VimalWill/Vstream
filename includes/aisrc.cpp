d010539b1d80fc1ea8370e5e4a4bd9f45a50c912
vimal <vimalwilliam99@gmail.com>
2023-06-11 12:09:37 +0530

    hardware detection function been updated

--- includes/aisrc.cpp
+++ includes/aisrc.cpp
@@ -1,16 +1,21 @@
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+#define CL_HPP_ENABLE_EXCEPTIONS
+
 #include <includes/aisrc.hpp>
 #include <gst/gst.h>
 #include <gst/app/app.h>
 #include <gst/app/gstappsrc.h>
-
 #include <CL/opencl.hpp>
 #include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
 
 static void GstAISrc::NeedSource(GstAppSrc* appsrc, gpointer user_data){
     //initalize the gst variable
     GstBuffer* buffer; 
     GstMapInfo map;
     GstFlowReturn ret; 
+
+    GstClockTime timestamp;
 
     //opencv source capture 
     cv::VideoCapture cap;
@@ -22,5 +27,33 @@ static void GstAISrc::NeedSource(GstAppSrc* appsrc, gpointer user_data){
         cap.read(frame); 
 
         //initalize the Inference Engine here 
+
     }
 }
+
+auto HardwareDetect() -> bool{
+
+    //get the opencl platform and context 
+    std::vector<cl::Platform> platforms; 
+    std::vector<cl::Device> platformDevices, ctxDevice;  
+    bool HW_FLG = false;
+    
+    try{
+        static cl_int err = cl::Platform(&platforms); 
+        if(err != CL_SUCCESS)
+            std::cerr << "no platform found"<< std::endl; 
+
+        platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &platformDevices); 
+        cl::Context context(platformDevices); 
+        ctxDevice = context.getInfo<CL_CONTEXT_DEVICES>(); 
+
+        //get the device name 
+        std::string deviceName = ctxDevice[0].getInfo<CL_DEVICE_NAME>();  
+        if(deviceName.find("NVIDIA") != std::string::npos){
+            std::cout << "Required Hardware Detetcted" << std::endl;
+            HW_FLG = true; 
+        } 
+    }
+
+}
+
