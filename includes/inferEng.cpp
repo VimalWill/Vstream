2717c85e88ff9801311e16ebf0d04ca3350bf73e
vimal <vimalwilliam99@gmail.com>
2023-06-12 18:47:57 +0530

    resize and color conversion updated

--- includes/inferEng.cpp
+++ includes/inferEng.cpp
@@ -9,8 +9,16 @@ cv::Mat GstInferaEng::InferenceEngine(cv::Mat &input_image){
     
 }
 
-cv::Mat GstInferaEng::InferenceEngine(cv::Mat &frame){
+cv::Mat GstInferaEng::Preproc(cv::Mat &frame){
     cv::Mat image = frame; 
+
+    //color conversion and resize
+    cv::Mat resized_img, processed_img;
     cv::cvtColor(image, image, cv::COLOR_BGR2RGB); 
-    
+    cv::resize(image, resized_img, cv::Size(480, 640)); 
+
+    //normalization
+
+
+
 }
