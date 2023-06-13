64a385157fce478c5da2efceb057d90a4b8cfd62
vimal <vimalwilliam99@gmail.com>
2023-06-13 15:05:24 +0530

    Onnx runtime session initalized

--- includes/inferEng.cpp
+++ includes/inferEng.cpp
@@ -1,15 +1,38 @@
 #include <includes/InferEng.hpp>
 #include <opencv2/opencv.hpp>
 #include <bits/stdc++.h>
+#include <onnxruntime/core/session/onnxruntime_cxx_api.h>
 
-cv::Mat GstInferaEng::Preproc(cv::Mat& image, const int InputWidth, const int InputHeight){
+GstInferaEng::GstInferaEng(cv::Mat& Image){
+    Input_img = Image;
+}
+
+cv::Mat GstInferaEng::InferenceEngine(){
+
+    std::string instanceName{"Gst-Object-Detection"};
+    std::string ModelPath{"/home/vimal/Edge AI/Vstream/model/yolov8n.onnx"};
+
+    //initalize the onnxruntime 
+    Ort::Env env(OrtLoggingLevel::ORT_LOGGING_LEVEL_WARNING, instanceName.c_str()); 
+    Ort::SessionOptions sessionOptions; 
+    sessionOptions.AppendExecutionProvider_CUDA(); 
+
+    //initalize the session
+    Ort::Session session(env, ModelPath, sessionOptions); 
+
+
+
+    
+}
+
+cv::Mat GstInferaEng::Preproc(cv::Mat& image){
 
     //Image preprocessing for YOLOv8 
     cv::Mat frame, frame_resize, processed_frame; 
     frame = image; 
 
     cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB); 
-    cv::resize(frame, frame_resize, cv::Size(InputHeight, InputHeight)); 
+    cv::resize(frame, frame_resize, cv::Size(InputWidth, InputHeight)); 
 
     cv::Mat image_data; 
     frame_resize.convertTo(image_data, CV_32FC3, 1.0/255.0); 
@@ -19,4 +42,18 @@ cv::Mat GstInferaEng::Preproc(cv::Mat& image, const int InputWidth, const int In
     return processed_frame; 
 }
 
-cv::
+cv::Mat GstInferaEng::Postproc(cv::Mat& outputImage){
+
+    //Image Post-processing includes Non-maximal supression and overlay function
+    cv::Mat frame = outputImage; 
+    frame = frame.reshape(0, {0});
+    cv::transpose(frame, frame); 
+
+    //get the no. of rows
+    int rows = frame.rows; 
+    
+    //calculate the scaling factor 
+
+
+
+}
