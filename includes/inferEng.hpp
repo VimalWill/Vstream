72af82c4114f368dee89dfe00445a2f764225c52
vimal <vimalwilliam99@gmail.com>
2023-06-25 01:09:59 +0530

    rewriting the inference engine

--- includes/inferEng.hpp
+++ includes/inferEng.hpp
@@ -1,26 +1,2 @@
-#ifndef INFERENG_HPP
-#define INFERENG_HPP
-
-//initalize the inference engine 
-#include <opencv2/opencv.hpp>
-#include <bits/stdc++.h>
-
-class GstInferaEng{
-    private:
-        int InputWidth; 
-        int InputHeight;
-
-        cv::Mat Input_img;
-
-    public:
-        GstInferaEng(cv::Mat& image);
-
-
-        auto InferenceEngine() -> cv::Mat; 
-        auto Preproc(cv::Mat) -> std::vector<float>;
-        auto Postproc(cv::Mat& outputImage) -> cv::Mat;
-        void letterBox(cv::Mat& Input_image, cv::Mat& OutputImage);
-
-};
-
-#endif /*INFERENG_HPP*/
+#pragma once 
+#ifdef 
